f27d3458127c21a5d44b120b662cc34e3450c0f9
Ira Bakumenko <ibakumenko997@gmail.com>
2021-11-01 23:01:18 +0300

    add header

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 import './Header.scss'
 
 const Header = () => {
     return (
         <div className="header">
-
+            <div className="header-logo">Bakum</div>
+            <div className="header-links">
+                <Link to="/blog">BLOG</Link>
+                <Link to="/about">ABOUT ME</Link>
+            </div>
         </div>
     )
 }
